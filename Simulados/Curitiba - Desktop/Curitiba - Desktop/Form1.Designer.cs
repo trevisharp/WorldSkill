e59636d14ca1eadb3a92e1ec308d6479f82828b2
moll <moll@alunos.utfpr.edu.br>
2022-07-10 22:42:47 -0300

    Tentando fazer a bosta do poligono e falhando miseravelmente

---
+++ Simulados/Curitiba - Desktop/Curitiba - Desktop/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Curitiba___Desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnPoligono = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(169, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(505, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // btnPoligono
+            // 
+            this.btnPoligono.Location = new System.Drawing.Point(692, 12);
+            this.btnPoligono.Name = "btnPoligono";
+            this.btnPoligono.Size = new System.Drawing.Size(96, 133);
+            this.btnPoligono.TabIndex = 1;
+            this.btnPoligono.Text = "poligono";
+            this.btnPoligono.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(726, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPoligono);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnPoligono;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Simulados/Curitiba - Desktop/Curitiba - Desktop/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Curitiba___Desktop
+{
+    public partial class Form1 : Form
+    {
+        Point? p;
+        Graphics g;
+        Bitmap bmp;
+        List<Point> points;
+        bool IsPolygon = false;
+        Color color = Color.Black;
+        Timer tm = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+
+            btnPoligono.Click += delegate
+            {
+                points = new List<Point>();
+                IsPolygon = true;
+                Cursor = Cursors.Cross;
+
+                //if(colorDialog1.ShowDialog() == DialogResult.OK){
+                //    color = colorDialog1.Color;
+                //}
+            };
+
+            Load += delegate
+            {
+                tm.Start();
+            };
+
+            tm.Interval = 25;
+            tm.Tick += delegate
+            {
+                pictureBox1.Refresh();
+            };
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        => p = e.Location;
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        => p = null;
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (IsPolygon == true)
+            {
+                Pen caneta = new Pen(color, 5);
+                points.Add(new Point(e.X, e.Y));
+
+                if(points.Count() > 1)
+                {
+                    label1.Text = $"{points[points.Count() - 1]} {points.Last()}";
+                    g.DrawLine(caneta, points[points.Count() - 1], points.Last());
+                }
+            }
+        }
+    }
+}
